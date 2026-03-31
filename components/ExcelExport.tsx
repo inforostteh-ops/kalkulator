62c68d7e80be4923f1c0a273a2751bfc5802f25a
inforostteh-ops <info.rostteh@gmail.com>
2026-03-31 20:59:35 +0300

    Create ExcelExport component to export calculations to Excel file

---
+++ components/ExcelExport.tsx
@@ -0,0 +1,16 @@
+import * as XLSX from 'xlsx';
+
+const ExcelExport = ({ data }) => {
+    const handleExport = () => {
+        const worksheet = XLSX.utils.json_to_sheet(data);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Calculations');
+        XLSX.writeFile(workbook, 'Calculations.xlsx');
+    };
+
+    return (
+        <button onClick={handleExport}>Export to Excel</button>
+    );
+};
+
+export default ExcelExport;
